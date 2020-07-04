5ef69f8a348f7b78f9e9fb69d449322edd0832c2
Leonardo Motta <leonardo.nogueira@coopersystem.com.br>
2020-07-04 16:48:28 -0300

    feat: list planets

--- src/pages/PlanetsList/index.tsx
+++ src/pages/PlanetsList/index.tsx
@@ -1,9 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { AxiosError } from 'axios';
+import { useLocation } from 'react-router-dom';
+
+import Swapi from '../../core/Swapi';
+import { Planets, Pagination } from '../../core/Swapi/interfaces';
+import { PlanetCard } from '../../components';
 
 const PlanetsList: React.FC = () => {
+  const { search: urlSearch } = useLocation();
+  const urlSearchParams = new URLSearchParams(urlSearch);
+
+  const [page] = useState(parseInt(urlSearchParams.get('page') || '1', 10));
+  const [search] = useState(urlSearchParams.get('search') || undefined);
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState<Pagination<Planets>>();
+  const [error, setError] = useState<AxiosError | undefined>();
+
+  useEffect(() => {
+    async function loadData() {
+      setLoading(true);
+
+      try {
+        const response = await Swapi.getAllPlanets({ page, search });
+
+        setData(response);
+      } catch (err) {
+        setError(err);
+      }
+
+      setLoading(false);
+    }
+
+    loadData();
+  }, [page, search]);
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (error) {
+    return <h1>error</h1>;
+  }
+
   return (
-    <div>
-      <h1>PlanetsList</h1>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        flexWrap: 'wrap',
+      }}
+    >
+      {data?.results.map(({ url, name, terrain, gravity, population }) => (
+        <PlanetCard
+          key={url}
+          url={url}
+          name={name}
+          terrain={terrain}
+          gravity={gravity}
+          population={population}
+        />
+      ))}
     </div>
   );
 };
