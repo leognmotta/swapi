0b84d7e4f8140acec2e6172f256ace98e2805685
Leonardo Motta <leonardo.nogueira@coopersystem.com.br>
2020-07-04 20:41:01 -0300

    feat: add search

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import ClipLoader from 'react-spinners/ClipLoader';
 
-const Search: React.FC = () => {
-  return <div />;
+import './index.scss';
+
+interface Props {
+  onSubmit: (text: string) => void;
+  loading: boolean;
+}
+
+const Search: React.FC<Props> = ({ onSubmit, loading }) => {
+  const [search, setSearch] = useState('');
+
+  return (
+    <div className="form-container">
+      <form
+        onSubmit={async e => {
+          e.preventDefault();
+
+          onSubmit(search);
+
+          setSearch('');
+        }}
+      >
+        <label htmlFor="search">
+          Search for planet:
+          <input
+            type="text"
+            id="search"
+            value={search}
+            onChange={e => setSearch(e.target.value)}
+          />
+        </label>
+        <button type="submit">
+          {loading ? <ClipLoader size={18} /> : <FaSearch size={18} />}
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Search;
--- src/pages/PlanetsList/index.tsx
+++ src/pages/PlanetsList/index.tsx
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { AxiosError } from 'axios';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useHistory } from 'react-router-dom';
 
 import Swapi from '../../core/Swapi';
 import { Planets, SwapiPagination } from '../../core/Swapi/interfaces';
 import { PlanetCard, RingLoader, Search, Pagination } from '../../components';
 
 const PlanetsList: React.FC = () => {
-  const { search: urlSearch } = useLocation();
-  const urlSearchParams = new URLSearchParams(urlSearch);
+  const { search: urlSearch, pathname } = useLocation();
+  const { push } = useHistory();
 
-  const [page] = useState(parseInt(urlSearchParams.get('page') || '1', 10));
-  const [search] = useState(urlSearchParams.get('search') || undefined);
+  const urlSearchParams = new URLSearchParams(urlSearch);
+  const page = parseInt(urlSearchParams.get('page') || '1', 10);
+  const search = urlSearchParams.get('search') || undefined;
+
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState<SwapiPagination<Planets>>();
   const [error, setError] = useState<AxiosError | undefined>();
@@ -34,36 +36,43 @@ const PlanetsList: React.FC = () => {
     loadData();
   }, [page, search]);
 
-  if (loading) {
-    return <RingLoader />;
-  }
-
   if (error) {
     return <h1>error</h1>;
   }
 
   return (
     <div>
-      <Search />
-
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          flexWrap: 'wrap',
+      <Search
+        loading={loading}
+        onSubmit={value => {
+          push(
+            `${pathname}?page=${page}${value !== '' ? `&search=${value}` : ''}`
+          );
         }}
-      >
-        {data?.results.map(({ url, name, terrain, gravity, population }) => (
-          <PlanetCard
-            key={url}
-            url={url}
-            name={name}
-            terrain={terrain}
-            gravity={gravity}
-            population={population}
-          />
-        ))}
-      </div>
+      />
+
+      {loading ? (
+        <RingLoader />
+      ) : (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            flexWrap: 'wrap',
+          }}
+        >
+          {data?.results.map(({ url, name, terrain, gravity, population }) => (
+            <PlanetCard
+              key={url}
+              url={url}
+              name={name}
+              terrain={terrain}
+              gravity={gravity}
+              population={population}
+            />
+          ))}
+        </div>
+      )}
 
       <Pagination />
     </div>
