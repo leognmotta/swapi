dd44c4d45db9dafc4689bbe2d05db319bedcafdf
Leonardo Motta <leonardo.nogueira@coopersystem.com.br>
2020-07-04 16:47:42 -0300

    feat: add base layout

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
+import logo from '../assets/star-wars-4.svg';
+
 import './index.scss';
 import './global.scss';
 
@@ -10,7 +12,18 @@ const Layout: React.FC = ({ children }) => (
       <meta name="description" content="Star Wars Planets." />
     </Helmet>
 
+    <header>
+      <img src={logo} alt="Star Wars" />
+    </header>
+
     {children}
+
+    <footer>
+      {`Made by `}
+      <a href="https://leomotta.me" target="__blank">
+        Leonardo Motta
+      </a>
+    </footer>
   </>
 );
 
