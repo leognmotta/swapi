d43b04fc27d665c4d7331f3e4b3b28512b6e3cce
Leonardo Motta <leonardo.nogueira@coopersystem.com.br>
2020-07-04 16:48:00 -0300

    feat: update Planets interface

--- src/core/Swapi/interfaces/index.d.ts
+++ src/core/Swapi/interfaces/index.d.ts
@@ -16,18 +16,18 @@ export interface Pagination<T> {
 }
 
 export interface Planets {
-  name: string;
-  rotation_period: string;
-  orbital_period: string;
-  diameter: string;
-  climate: string;
-  gravity: string;
-  terrain: string;
-  surface_water: string;
-  population: string;
-  residents: string[];
-  films: string[];
-  created: Date;
-  edited: Date;
-  url: string;
+  name?: string;
+  rotation_period?: string;
+  orbital_period?: string;
+  diameter?: string;
+  climate?: string;
+  gravity?: string;
+  terrain?: string;
+  surface_water?: string;
+  population?: string;
+  residents?: string[];
+  films?: string[];
+  created?: Date;
+  edited?: Date;
+  url?: string;
 }
